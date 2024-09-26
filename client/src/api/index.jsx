daa55f725757c9f03dbbf20242241c819490f4ad
hariram4603 <hariram4603@gmail.com>
2024-09-26 21:45:09 +0530

    link

--- client/src/api/index.jsx
+++ client/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fitnesstracker-seven.vercel.app/api/",
+  baseURL: "https://fitnesstracker-chi.vercel.app//api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
